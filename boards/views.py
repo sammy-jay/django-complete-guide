7b07f8c471236a4f9b9bb7cf0b607cc851eefce4
Tolu Soneye <sammyjayden27@gmail.com>
2022-08-29 19:02:00 +0000

    added the home view

--- boards/views.py
+++ boards/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Board
 
 def home(request):
-    return HttpResponse("Hello, world")
+    boards = Board.objects.all()
+    context = {
+        'boards': boards,
+    }
+    return render(request, 'home.html', context)
